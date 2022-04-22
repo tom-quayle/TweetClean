213bb8ff2932b5dde3f86540c60d6932cf472dea
Tom Quayle <tomquayle@toms-mbp.home>
2022-04-22 13:12:37 +0100

    added Manual document, changed form to not accept no input

--- mysite/GUI/forms.py
+++ mysite/GUI/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SearchTwitter(forms.Form):
-    name = forms.CharField(label="Enter Your Twitter Name Here...", max_length=25)
+    name = forms.CharField(label="", max_length=25)
 
     # this file can be deleted if we are not using Django's premade forms
